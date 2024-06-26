82a07adb5ec099dec365d535a8b0aab414f5749e
skwangles <tree_house@Hotmail.co.nz>
2024-06-26 15:40:06 +1200

    Add gitignore to avoid .out

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 echo "Compiling ProcessFiles"
 
-g++ ProcessFiles.cpp -o ProcessFiles
+g++ ProcessFiles.cpp -o ProcessFiles.out
 
 echo "Running ProcessFiles"
 
 # add JSON directory of phone to end if you want to use the JSON files
-./ProcessFiles /media/skwangles/disk/DCIM/Camera01/ /media/skwangles/KINGSTON/ProcessedMedia/
+./ProcessFiles.out /media/skwangles/disk/DCIM/Camera01/ /media/skwangles/KINGSTON/ProcessedMedia/
